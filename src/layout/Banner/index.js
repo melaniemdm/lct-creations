3cc57fc66ed16bf68462295c0958fef0150fac8b
melaniemdm <melanieborja.pro@gmail.com>
2022-05-11 21:34:55 +0200

    fix(css & js) banner

--- src/layout/Banner/index.js
+++ src/layout/Banner/index.js
@@ -7,8 +7,9 @@ function Banner(){
 <div className="containerBanner">
     
 <div id="backgroundBanner"></div>
-<div className="titleBanner1">Nous sommes à votre écoute! </div>
-<div className="titleBanner2">Pour concrétiser vos projets! </div>
+<div className="titleBanner title1">Vous avez un projet en tête? </div>
+<div className="titleBanner title2">Nous sommes à votre écoute! </div>
+<div className="titleBanner title3">Pour concrétiser vos projets! </div>
 </div>  
  
     );
